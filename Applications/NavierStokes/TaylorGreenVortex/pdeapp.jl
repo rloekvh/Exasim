321c5120fb5c4c4c599a3254438b768767f69a09
rloekvh <rloekvh@gmail.com>
2021-02-19 18:14:27 -0500

    slightly tweaked pdeapp files

--- Applications/NavierStokes/TaylorGreenVortex/pdeapp.jl
+++ Applications/NavierStokes/TaylorGreenVortex/pdeapp.jl
@@ -1,5 +1,5 @@
 # specify an Exasim version to run
-version = "Version0.1";
+version = "Version0.3";
 
 # External packages
 using Revise, DelimitedFiles, SymPy
@@ -19,10 +19,12 @@ pde.model = "ModelD";            # ModelC, ModelD, ModelW
 include("pdemodel.jl");          # include the PDE model file
 
 # Set discretization parameters, physical parameters, and solver parameters
+# NOTE: useful parameters
 pde.porder = 3;          # polynomial degree
 pde.torder = 3;          # time-stepping order of accuracy
 pde.nstage = 3;          # time-stepping number of stages
-pde.dt = 0.05*ones(200);   # time step sizes
+pde.dt = 0.05*ones(100);   # time step sizes
+#pde.dt = 0.05*ones(20);
 pde.soltime = collect(1:length(pde.dt)); # steps at which solution are collected
 pde.visdt = 0.05; # visualization timestep size
 
@@ -41,21 +43,26 @@ pde.NLiter=2;
 # Choose computing platform and set number of processors
 #pde.platform = "gpu";           # choose this option if NVIDIA GPUs are available
 pde.mpiprocs = 1;                # number of MPI processors
+pde.gpucompiler = "/home/software/cuda/9.0/bin/nvcc"
+#pde.gpucompiler = "/home/software/spack/llvm/7.0.1-ggobltsozzkw3rqjnslhigxqkjxrafnh/bin/clang++";
+#pde.gpuflags = " --cuda-path=/path/to/cuda/version "
+pde.gpuflags = "-Wl,-rpath, -I/home/software/cuda/9.0/include -L/home/software/cuda/9.0/lib64 -lcudart -lcublas";
+pde.cpuflags = pde.cpuflags * " -g -pg"
 
 # create a linear mesh for a cube domain
-mesh.p, mesh.t = Mesh.cubemesh(16,16,16,1);
+#mesh.p, mesh.t = Mesh.cubemesh(16,16,16,1);
+mesh.p, mesh.t = Mesh.cubemesh(8,8,8, 1);
 mesh.p = 2*pi*mesh.p;
 # expressions for disjoint boundaries
 mesh.boundaryexpr = [p -> (p[2,:] .< 1e-3), p -> (p[1,:] .> 2*pi-1e-3), p -> (p[2,:] .> 2*pi-1e-3), p -> (p[1,:] .< 1e-3), p -> (p[3,:] .< 1e-3), p -> (p[3,:] .> 2*pi-1e-3)];
 mesh.boundarycondition = [1 1 1 1 1 1]; # Set boundary condition for each disjoint boundary
 mesh.periodicexpr = [2 p->p[2:3,:] 4 p->p[2:3,:]; 1 p->p[[1,3],:] 3 p->p[[1,3],:]; 5 p->p[[1,2],:] 6 p->p[[1,2],:]];
 
+# if you just want to generate code to send to a cluster
+#compilerstr, pde, mesh, master, dmd = Postprocessing.producecode(pde, mesh);
+
 # call exasim to generate and run C++ code to solve the PDE model
 sol, pde, mesh,~,~,~,~  = Postprocessing.exasim(pde,mesh);
 
-# visualize the numerical solution of the PDE model using Paraview
-pde.visscalars = ["density", 1, "energy", 5];  # list of scalar fields for visualization
-pde.visvectors = ["momentum", [2, 3, 4]]; # list of vector fields for visualization
-Postprocessing.vis(sol,pde,mesh); # visualize the numerical solution
 print("Done!");
 
--- Applications/Poisson/Poisson3d/pdeapp.jl
+++ Applications/Poisson/Poisson3d/pdeapp.jl
@@ -1,5 +1,5 @@
 # specify an Exasim version to run
-version = "Version0.1";
+version = "Version0.3";
 
 # External packages
 using Revise, DelimitedFiles, SymPy
@@ -19,16 +19,20 @@ pde.model = "ModelD";            # ModelC, ModelD, ModelW
 include("pdemodel.jl");          # include the PDE model file
 
 # Set discretization parameters, physical parameters, and solver parameters
-pde.porder = 3;                  # polynomial degree
+pde.porder = 2;                  # polynomial degree
 pde.physicsparam = [1.0 0.0];    # thermal conductivity and boundary value
 pde.tau = [1.0];                 # DG stabilization parameter
 
 # Choose computing platform and set number of processors
 #pde.platform = "gpu";           # choose this option if NVIDIA GPUs are available
-pde.mpiprocs = 2;                # number of MPI processors
+#pde.gpucompiler = "/home/software/spack/llvm/7.0.1-ggobltsozzkw3rqjnslhigxqkjxrafnh/bin/clang++"
+pde.gpucompiler = "/home/software/cuda/9.0/bin/nvcc"
+#pde.gpucompiler = "/home/software/cuda/10.2/bin/nvcc"
+pde.mpiprocs = 1;                # number of MPI processors
+pde.gpuflags = "-Wl,-rpath, -I/home/software/cuda/10.2/include -L/home/software/cuda/10.2/lib64 -lcudart -lcublas";
 
 # create a linear mesh of 8 by 8 by 8 quadrilaterals for a unit cube
-mesh.p,mesh.t = Mesh.cubemesh(8,8,8,1);
+mesh.p,mesh.t = Mesh.cubemesh(4,4,4,1);
 # expressions for domain boundaries
 mesh.boundaryexpr = [p -> (p[2,:] .< 1e-3), p -> (p[1,:] .> 1-1e-3), p -> (p[2,:] .> 1-1e-3), p -> (p[1,:] .< 1e-3), p -> (p[3,:] .< 1e-3), p -> (p[3,:] .> 1-1e-3)];
 mesh.boundarycondition = [1 1 1 1 1 1]; # Set boundary condition for each disjoint boundary
@@ -38,12 +42,12 @@ sol, pde, mesh, ~,~,~,~  = Postprocessing.exasim(pde,mesh);
 
 # visualize the numerical solution of the PDE model using Paraview
 #pde.paraview = "/Applications/ParaView-5.8.1.app/Contents/MacOS/paraview"; # Set the path to Paraview executable
-pde.visscalars = ["temperature", 1];  # list of scalar fields for visualization
-pde.visvectors = ["temperature gradient", [2, 3, 4]]; # list of vector fields for visualization
-mesh.dgnodes = Postprocessing.vis(sol,pde,mesh); # visualize the numerical solution
-x = mesh.dgnodes[:,1,:]; y = mesh.dgnodes[:,2,:]; z = mesh.dgnodes[:,3,:];
-uexact = sin.(pi*x).*sin.(pi*y).*sin.(pi*z); # exact solution
-uh = sol[:,1,:];  # numerical solution
-maxerr = maximum(abs.(uh[:]-uexact[:]));
-print("Maximum absolute error: $maxerr\n");
+#pde.visscalars = ["temperature", 1];  # list of scalar fields for visualization
+#pde.visvectors = ["temperature gradient", [2, 3, 4]]; # list of vector fields for visualization
+#mesh.dgnodes = Postprocessing.vis(sol,pde,mesh); # visualize the numerical solution
+#x = mesh.dgnodes[:,1,:]; y = mesh.dgnodes[:,2,:]; z = mesh.dgnodes[:,3,:];
+#uexact = sin.(pi*x).*sin.(pi*y).*sin.(pi*z); # exact solution
+#uh = sol[:,1,:];  # numerical solution
+#maxerr = maximum(abs.(uh[:]-uexact[:]));
+#print("Maximum absolute error: $maxerr\n");
 print("Done!");
